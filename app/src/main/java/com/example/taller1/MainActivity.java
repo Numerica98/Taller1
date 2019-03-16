8d57671fcfdf234d28f7ae7268b6e1c5b5769c07
Elsy Chavez <00125717@gmail.com>
2019-03-16 10:50:43 -0600

    Ya con funcionalidad

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.taller1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,10 +20,23 @@ public class MainActivity extends AppCompatActivity {
         boton1 = findViewById(R.id.boton_entran);
         boton2 = findViewById(R.id.boton_salen);
         texto1 = findViewById(R.id.texto_entran);
-        texto1 = findViewById(R.id.texto_entran);
+        texto2 = findViewById(R.id.texto_salen);
         cont1 = 0;
         cont2 = 0;
 
-        
+        boton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                cont1 = cont1 + 1;
+                texto1.setText(String.valueOf(cont1));
+        }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                cont2 = cont2 + 1;
+                texto2.setText(String.valueOf(cont2));
+            }
+        });
+
     }
 }
