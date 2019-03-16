6392eb675297815d588281ec9ab78a3a498b9a49
Elsy Chavez <00125717@gmail.com>
2019-03-16 10:19:35 -0600

    Parte grafica terminada

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.taller1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton1, boton2;
+    TextView texto1, texto2;
+    int cont1,cont2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
